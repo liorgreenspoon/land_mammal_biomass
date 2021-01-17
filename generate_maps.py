1d1f53054430c94bd3620714de5e22f30cfed6d9
liorgreenspoon <lior.greenspoon@gmail.com>
2021-01-17 16:42:53 +0200

    Percantages added to map

--- generate_maps.py
+++ generate_maps.py
@@ -41,12 +41,12 @@ def get_data(n):
     return range_polygons
 
 
-def get_continent_data():
-    world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
-    world = world[world.continent != 'Antarctica']
-    continents = world.dissolve (by='continent')
-    continents = continents.to_crs("EPSG:6933")
-    return continents
+# def get_continent_data():
+#     world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+#     world = world[world.continent != 'Antarctica']
+#     continents = world.dissolve(by='continent')
+#     continents = continents.to_crs("EPSG:6933")
+#     return continents
 
 
 def get_continent_data_from_file():
@@ -70,7 +70,7 @@ def gen_grid(delta):
     Input: tuple, (x,y) size of cell in m^2
     Output:  a global grid cropped by continent polygons.
     """
-    outline = get_continent_data()
+    outline = get_continent_data_from_file()
     xmin, ymin, xmax, ymax = outline.total_bounds
     dx, dy = delta
     xgrid, ygrid = np.meshgrid(np.arange(xmin, xmax, dx), np.arange(ymin, ymax, dy))
@@ -88,7 +88,6 @@ def gen_grid(delta):
     grid_clip = grid_clip[grid_clip.geom_type != 'MultiLineString']
     grid_clip['grid_index'] = grid_clip.index
     grid_clip.grid_index = grid_clip['grid_index'].apply(str)
-
     return grid_clip
 
 
@@ -102,10 +101,9 @@ def overlay_and_sum_grid(num_of_species, delta=(500000, 500000)):
     print("overlay_and_sum done")
     inter_grouped = inter.groupby(['grid_index']).sum()
     inter_grouped = gpd.GeoDataFrame(inter_grouped.merge(grid, on='grid_index'))
-    inter_grouped = inter_grouped.assign(area_km_2=inter_grouped.geometry.area * 10 ** (-6))
-    inter_grouped = inter_grouped.assign(total_mass_kg=inter_grouped.total_mass * 10 ** (-3))
-    inter_grouped = inter_grouped.assign(total_mass_kg_km2=inter_grouped.total_mass_kg /
-                                                           inter_grouped.area_km_2)
+    inter_grouped = inter_grouped.assign(area_km_2=inter_grouped.geometry.area * 10 ** (-6),
+                                         total_mass_kg=inter_grouped.total_mass * 10 ** (-3))
+    inter_grouped = inter_grouped.assign(total_mass_kg_km2=inter_grouped.total_mass_kg / inter_grouped.area_km_2)
     return inter_grouped
 
 
@@ -144,19 +142,54 @@ def overlay_and_sum(geo_data, layer):
 def gen_grid_plot(gridded_data, log10=False):
     continents_polygon = gpd.GeoDataFrame(get_continent_data_from_file()).rename(columns={0: "geometry"})
     continents_polygon.crs = "EPSG:6933"
+    continents_polygon['continents_dissolve'] = pd.Series(
+        data=['Eurasia', 'North America', 'Eurasia', 'Africa', 'South America', 'Oceania', 'Australia'],
+        name='continent_edited')
+    continents_polygon = continents_polygon.dissolve(by='continents_dissolve', aggfunc='sum')
+    # plt.rcParams["figure.figsize"] = (30, 10)
+    # font = {'weight': 'normal',
+    #         'size': 40}
+    # plt.rc('font', **font)
+    # fig, ax = pyplot.subplots(1, 1)
+    # ax.axis('off')
+    # cmap = plt.cm.get_cmap('gist_earth').reversed()
+    # # cmap = gen_custom_cmap()
+    # ax.set_label('verbosity coefficient')
+    # ax.set_title('Wild Mammal Mass Density')
+    # base = gridded_data.plot(column='total_mass_kg_km2' ,ax=ax, legend=True, cmap=cmap,
+    #                          legend_kwds={'label': r"$kg/km^2$"}, )
+    # continents_polygon.plot(ax=base, fc='none', ec='grey', linewidth=0.3)
     plt.rcParams["figure.figsize"] = (30, 10)
+
     font = {'weight': 'normal',
-            'size': 40}
+            'size': 30}
     plt.rc('font', **font)
     fig, ax = pyplot.subplots(1, 1)
     ax.axis('off')
-    cmap = plt.cm.get_cmap('gist_earth').reversed()
-    # cmap = gen_custom_cmap()
+
+    cmap = gen_custom_cmap()
     ax.set_label('verbosity coefficient')
     ax.set_title('Wild Mammal Mass Density')
-    base = gridded_data.plot(column='total_mass_kg_km2' ,ax=ax, legend=True, cmap=cmap,
-                             legend_kwds={'label': r"$kg/km^2$"}, )
+    base = gridded_data.plot(column='total_mass_kg_km2', ax=ax, legend=True, cmap=cmap,
+                                    legend_kwds={'label': r"$kg/km^2$"})
+
+    delta = (0, 7 * 10 ** 5)
+
+    for row in range(0, 5):
+        continent_text_df = pd.read_csv('continent_text_for_plot.csv')
+        base.annotate(text=continent_text_df['continents_dissolve'].iloc[row],
+                      xy=eval(continent_text_df['loc'].iloc[row]),
+                      ha='left', size=20, weight='bold')
+        base.annotate(text='Total: ' + str(continent_text_df['continent_mass'].iloc[row]) + ' Mt',
+                      xy=np.subtract(eval(continent_text_df['loc'].iloc[row]), np.multiply(delta, (1, 1))),
+                      ha='left', size=20)
+        base.annotate(str(continent_text_df['percent_contribution'].iloc[row])+ '% ' +
+                      continent_text_df['conributor_names'].iloc[row],
+                      xy=np.subtract(eval(continent_text_df['loc'].iloc[row]),
+                                     np.multiply(delta, (1, 2))), ha='left', size=20)
+
     continents_polygon.plot(ax=base, fc='none', ec='grey', linewidth=0.3)
+    fig.savefig('mammal_by_pixel.png', format='png')
 
 
 def plot_mass_density_map_from_file():
@@ -171,14 +204,13 @@ def gen_custom_cmap():
     """
     Output:  customized colormap.
     """
-    # each row is (z-value, hue, saturation, value/brightness)
     values = [
         (0.00, 0.15, 0.0, 1.0),
-        (0.05, 0.15, 0.5, 0.9),
-        (0.10, 0.4, 0.5, 0.8),
-        (0.15, 0.4, 0.5, 0.7),
-        (0.20, 0.2, 0.5, 0.6),
-        (0.40, 0.1, 0.5, 0.5),
+        (0.1, 0.15, 0.5, 0.9),
+        (0.15, 0.4, 0.5, 0.8),
+        (0.20, 0.4, 0.5, 0.7),
+        (0.40, 0.2, 0.5, 0.6),
+        (0.50, 0.1, 0.5, 0.5),
         (0.80, 0.1, 0.5, 0.4),
         (1.00, 0.0, 1.0, 0.0)
     ]
@@ -191,4 +223,33 @@ def gen_custom_cmap():
     cmap = LinearSegmentedColormap('mammals', segmentdata=cdict, N=256)
     return cmap
 
+
+# from datetime import datetime
+# start = datetime.now().time()
+# print("start =", start)
+# continents = overlay_and_sum_continent(4900)
+# end = datetime.now().time()
+# continents['total_mass_Mt_'] = continents.total_mass_Mt*(32.7/37.49)
+# print("end =", end)
+#
+# continents['cont'] = ['Africa','Eurasia','Australia and Oceania','Eurasia','North America','Australia and Oceania','South America']
+# continents = continents.dissolve(by='cont', aggfunc='sum')
+# continents['conributor_names'] = ['African Elephant','Eastern Grey Kangaroo','Roe deer', 'White-tailed deer', 'Capybara']
+# continents['conributor_mass'] = [2.1,0.7,0.5,2,0.9]
+# mult_x = 10**7
+# mult_y = 10**6
+# continents['loc'] = [(-0.1*mult_x,-4.2*mult_y),
+#                      (1.0*mult_x,-5.3*mult_y),
+#                      (1.35*mult_x,2.5*mult_y),
+#                      (-1.8*mult_x,5*mult_y),
+#                      (-1.4*mult_x,-0.0*mult_y)]
+#
+#
+# continents['continent_mass'] = np.round(continents.total_mass_Mt_,1)
+# continents['percent_contribution'] = np.round(continents.conributor_mass/continents.continent_mass*100).astype(int).astype(str)
+# continents['continent_mass'] = continents['continent_mass'].astype(str)
+# continent_text_df = continents[['conributor_names','conributor_mass','loc','continent_mass',
+#                                 'percent_contribution']]
+# continent_text_df = continent_text_df.reset_index()
+# continent_text_df.to_csv('continent_text_for_plot.csv')
 
